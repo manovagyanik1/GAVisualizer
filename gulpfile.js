e8128bc325aac47daca19743b713b0c70cc19684
Shubham Agrawal <shubham@headout.com>
2017-02-18 02:10:06 +0530

    adding demo js file

--- gulpfile.js
+++ gulpfile.js
@@ -42,7 +42,7 @@ gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
         server: {
             baseDir: '_site'
         },
-        notify: false
+        notify: true
     });
 });
 
